954a70257c2d93d8aacfcd85a5ab7ff2b74bb685
Satish Goda <satishgoda@live.com>
2016-10-14 22:15:49 +0800

    cmds.polyColorPerVertex
    http://help.autodesk.com/cloudhelp/2016/ENU/Maya-Tech-Docs/CommandsPython/polyColorPerVertex.html

---
+++ documentation/commands/polyColorPerVertex.py
@@ -0,0 +1,7 @@
+"""
+polyColorPerVertex -r 0.872 -g 0.067 -b 0;
+polyColorPerVertex -r 0.872 -g 0.067 -b 0;
+polyColorPerVertex -r 0.872 -g 0.067 -b 0;
+polyColorPerVertex -r 0.872 -g 0.067 -b 0;
+polyColorPerVertex -r 0.872 -g 0.067 -b 0;
+"""
